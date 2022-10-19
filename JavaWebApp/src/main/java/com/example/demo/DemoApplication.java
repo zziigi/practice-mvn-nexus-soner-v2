ab270f971e6a51e4e1325205a5cf9ce08a11d7d8
Ozie Iyamabo <francis.iyamabo@yahoo.com>
2022-10-18 20:10:24 -0600

    updated code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,17 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		String name = "vamsi";
+
+		System.out.println(name);
+
+		Integer age = "20";
+		System.out.println(age + 5);
+
+	}
+
+}
+
 	}
 
 }
